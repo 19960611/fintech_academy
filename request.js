f4fabcddb9131f97487579142a09328cdb240a14
jangaemin <papace@naver.com>
2020-10-21 21:57:37 +0900

    node.js request 실습

--- request.js
+++ request.js
@@ -1,5 +1,26 @@
-const request = require('request');
-request('http://www.naver.com', function (error, response, body) {
-    //네이버에 요청을 하고 받아온 데이터는 콜백함수의 3번째 파라미터인 바디에 넘겨져있음
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+const request = require("request");
+var parseString = require("xml2js").parseString;
+
+function getNewsData() {
+  request(
+    "http://newsapi.org/v2/top-headlines?apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5&country=kr&category=business",
+    function (error, response, body) {
+      var parsedData = JSON.parse(body);
+      //JSON String 으로 되어있는 데이터를 Js Object 변환
+      //#work2 Title 목록만 조회하기 (for, object select)
+      for (let index = 0; index < parsedData.articles.length; index++) {
+        const element = parsedData.articles[index];
+        console.log(index + 1, element.title);
+      }
+    }
+  );
+}
+
+request(
+  "http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109",
+  function (error, response, body) {
+    parseString(body, function (err, result) {
+      console.dir(result.rss.channel[0].item[0].description[0].header[0].wf[0]);
+    });
+  }
+);
